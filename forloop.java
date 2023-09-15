fd885ae8756b823b03dac96bd61806cf1df18356
sankapapa <pavankumar8485@gmail.com>
2023-09-15 20:27:50 +0530

    forloop

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop{
+public static void main(String[] args) {
+		System.out.println("numbers from 1 to 10");
+		for(int a=1;a<=10;a++) {
+			
+			System.out.println(a);
+	}
+	}
+	}
